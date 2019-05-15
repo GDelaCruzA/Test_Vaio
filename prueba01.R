530b6590561e38234b5fdf5ae900404fe215633e
GDelaCruzA <gaguero@ipn.mx>
2019-05-15 10:53:58 -0600

    Se agrega script para probar configuracion SSH

---
+++ prueba01.R
@@ -0,0 +1,11 @@
+# R script agregado para verificar la configuración y el acceso a Github
+# RStudio funciona como el SSH agent frente al servidor solo dando de alta
+# la clave ssh pública en el sitio.
+# 
+# Verificar si puede distinguir diferentes accesos desde clientes distintos
+# con su propia clave ssh generada localmente por el mismo RStudio
+# Salele
+# 
+citation()
+
+R.Version()
